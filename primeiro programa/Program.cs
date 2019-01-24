16f197159b03af36d210a2ae2c230811a1222cd4
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 14:58:54 -0200

    "sempre cirar nova pasta e subir dotnet new console

---
+++ primeiro programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiro_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compilou");
+        }
+    }
+}
