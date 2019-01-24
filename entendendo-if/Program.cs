1d4daafbf15a94c1acfaabd2980f0cb1fb1f492d
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 16:16:28 -0200

    if feito

---
+++ entendendo-if/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace entendendo_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fazendo um if");
+
+            int idade = 19;
+             if (idade > 18)
+             {
+                 Console.WriteLine("Pode entrar");
+             }
+             else
+             {
+                 Console.WriteLine("volte para casa");
+             }
+        }
+    }
+}
