28a8b35bad2985d246d00844c303d004fd50f237
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 15:26:50 -0200

    casting

---
+++ convertendo-variaveis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace convertendo_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" realisando cast de variaveis numericas");
+            double horas = 15.22;
+            Console.WriteLine("sao " + horas + " horas");
+            Console.WriteLine(" ou sendo menos preciso " + (int) horas + " horas");
+        }
+    }
+}
