4ee7a09e356a15f2f3e78dd61fa946a83c0da8a1
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 16:44:50 -0200

    for lace

---
+++ laco-for/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace laco_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("laco for");
+            double valorInvestido = 1000.00;
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes ++)
+            {
+                valorInvestido = valorInvestido + valorInvestido*0.0036;
+            }
+            Console.WriteLine(valorInvestido);
+        }
+    }
+}
