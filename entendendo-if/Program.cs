6cdde3ad09e1ad69f0942366ac3c12c132f20c2f
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 16:21:21 -0200

    ifs encadeados

--- entendendo-if/Program.cs
+++ entendendo-if/Program.cs
@@ -8,14 +8,21 @@ namespace entendendo_if
         {
             Console.WriteLine("Fazendo um if");
 
-            int idade = 19;
+            int idade = 14;
+            int numeroPessoas = 1;
              if (idade > 18)
              {
                  Console.WriteLine("Pode entrar");
              }
              else
              {
-                 Console.WriteLine("volte para casa");
+                 if(numeroPessoas >= 2)
+             {
+                 Console.WriteLine("Sejam bem vindos");
+             }
+             else{
+                Console.WriteLine("volte para casa");
+             }
              }
         }
     }
