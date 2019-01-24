ecffda97582094e25a1d20dbab145a6e898f31a1
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 15:46:07 -0200

    char e string

---
+++ strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Char e Strings");
+            char primeiraLetra = 'A';
+            Console.WriteLine(primeiraLetra);
+            Console.WriteLine((char) (primeiraLetra + 1));
+            string titulo = "Alura cursos de tecnologia";
+            Console.Write(titulo);
+
+        }
+    }
+}
