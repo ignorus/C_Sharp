51562af2fb32b4d977ddb8eafb90e4d95ce82c51
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 16:10:50 -0200

    atribuicao valores variavel

---
+++ variavel-in-variavel/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace variavel_in_variavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 23;
+            int idaderef = idade;
+            idade += 10;
+            Console.WriteLine(idaderef);
+            Console.WriteLine(idade);
+        }
+    }
+}
