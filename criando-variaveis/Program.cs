fe1a65d9ce5fa7979c2faea64c31063e667f37af
Tiago Bugan <tiago.debs@gmail.com>
2019-01-24 15:17:57 -0200

    int e double

---
+++ criando-variaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace criando_variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("projeto 2 criando variaveis numericas");
+            int idade = 23;
+            Console.WriteLine(idade);
+            double idadeReal = 23.8;
+            Console.WriteLine("sua idade real eh " + idadeReal);
+            Console.WriteLine("Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
