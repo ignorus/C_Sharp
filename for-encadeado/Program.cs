9ce83776ca4ba420cea90cae550750d38f359ff0
Tiago Bugan <tiago.debs@gmail.com>
2019-01-28 07:49:05 -0200

    for encadeado

---
+++ for-encadeado/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace for_encadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for encadeado");
+            double fatorRendimento = 1.0036;
+            double valorInvestido = 1000.0;
+
+            for(int contadorAno = 1; contadorAno <= 5; contadorAno ++)
+            {
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes ++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+            Console.WriteLine("Ao termino do investimento voce tera R$ " + valorInvestido);
+        }
+    }
+}
