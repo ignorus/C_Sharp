d63d329baeb2c1327d0f59a55b60aacb0446a6af
Tiago Bugan <tiago.debs@gmail.com>
2019-01-28 08:17:19 -0200

    atriuindo valopres a todas as variaveis do objeto

--- criando-classe/Program.cs
+++ criando-classe/Program.cs
@@ -9,6 +9,14 @@ namespace criando_classe
            ContaCorrente contaTiago = new ContaCorrente();
            contaTiago.titular = "Tiago";
            Console.WriteLine("O titular da conta e " + contaTiago.titular);
-         }
+           contaTiago.numeroAgencia = 1000;
+           Console.WriteLine("A agencia da conta e " + contaTiago.numeroAgencia);
+           contaTiago.numeroConta = 12345;
+           Console.WriteLine("O numero da conta e " + contaTiago.numeroConta);
+           contaTiago.saldo = 10.0;
+           Console.WriteLine("O saldo atual da conta e " + contaTiago.saldo);
+           contaTiago.saldo += 40.0;
+           Console.WriteLine("O saldo atual da conta e " + contaTiago.saldo);
+        }
     }
 }
