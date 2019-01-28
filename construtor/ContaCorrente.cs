9046dbfe919e2e850b8312012bd275c7f5444f16
Tiago Bugan <tiago.debs@gmail.com>
2019-01-28 10:40:57 -0200

    criando construtor

---
+++ construtor/ContaCorrente.cs
@@ -0,0 +1,62 @@
+namespace construtor
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular{get;set;}
+        public int NumeroAgencia{get;set;}
+        public int NumeroConta {get;set;}
+        private double saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if(value < 0)
+                {}
+                saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            NumeroAgencia = agencia;
+            NumeroConta = numero;
+        }
+
+        public bool Saque (double valor)
+        {
+            if(this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar (double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir (double valor, ContaCorrente contaDestino)
+        {
+            if(this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
