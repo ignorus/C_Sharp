78a7db052b960ae7b0375b43c23f2d0d45d4b0a2
Tiago Bugan <tiago.debs@gmail.com>
2019-01-29 15:06:13 -0200

    NullRef Exception

--- criando-excecoes/Program.cs
+++ criando-excecoes/Program.cs
@@ -7,8 +7,40 @@ namespace construtor
         static void Main(string[] args)
         {
             Console.WriteLine("Excecoes");
-            ContaCorrente conta = new ContaCorrente(867,1000);
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            try
+            {
+                Metodo();
+            }
+            catch(NullReferenceException erro)
+            {
+                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine(erro.Message);
+                Console.WriteLine("excecao tratada");
+            }
+        }
+
+        static void Metodo()
+        {
+            TestarDivisao(0);
+        }
+
+        static void TestarDivisao(int divisor)
+        {
+            try{
+            Dividir(10,divisor);
+            }
+            catch(DivideByZeroException erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+
+            return numero/divisor;
         }
     }
 }
