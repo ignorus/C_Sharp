e81a7d96e2e48b79f9b165c633699578223cfc65
Tiago Bugan <tiago.debs@gmail.com>
2019-01-30 13:59:30 -0200

    primeiro teste rodando em C#

---
+++ ler-arquivo-texto/Testar.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace ler_arquivo_texto 
+{
+    [TestFixture]
+    public class teste
+    {
+        [Test]
+        public void testar()
+        {
+            Console.WriteLine("Rodou meu teste");
+        }
+    }
+}
